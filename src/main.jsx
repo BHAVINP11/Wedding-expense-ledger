8fb90fa81286e14e249936c54d65c145be8e9486
BHAVINP11 <76607725+BHAVINP11@users.noreply.github.com>
2026-07-29 10:42:03 +0000

    Ui update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const pointerFine = window.matchMedia('(pointer: fine)')
+const applyCursorPreference = () => {
+  document.documentElement.classList.toggle('has-custom-cursor', pointerFine.matches)
+}
+
+applyCursorPreference()
+pointerFine.addEventListener?.('change', applyCursorPreference)
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
